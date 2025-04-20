35399012546932b6573470f1b5914054bf9bc753
Ricardo Garcés <rgarces2019@alu.uct.cl>
2025-04-20 01:12:07 -0400

    Actualización de archivos package-lock.json e index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom'; // Asegúrate de importar BrowserRouter
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
